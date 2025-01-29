c200d2155d3f6e46b580f3e2e33a2db2509dd326
jbelascoain <cuentasjuani@gmail.com>
2025-01-29 00:18:47 -0300

    (fix) function order

--- remove/delete.sh
+++ remove/delete.sh
@@ -3,8 +3,23 @@ show_menu() {
     echo "Select Agent or Server to remove"
     echo "1) Agent"
     echo "2) Server"
-    echo "3) Back"
+    echo "3) Exit"
     echo
+}
+
+remove_agent() {
+    sudo systemctl stop telegraf.service
+    sudo apt-get remove -y telegraf
+    sudo rm -rf /var/lib/telegraf /etc/telegraf
+}
+
+remove_server() {
+    sudo systemctl stop influxd.service grafana-server.service telegraf.service
+    sudo apt-get remove -y telegraf influxdb2 grafana
+
+    sudo rm -rf /var/lib/influxdb /var/log/influxdb
+    sudo rm -rf /var/lib/telegraf /etc/telegraf
+    sudo rm -rf /var/lib/grafana /etc/grafana /var/log/grafana 
 }
 
 
@@ -13,10 +28,10 @@ while true; do
     show_menu
     read -p "Enter your choices: " -a choices
 
-    # if [[ " ${choices[@]} " =~ " 3 " ]]; then
-    #     echo "Exiting..."
-    #     break
-    # fi
+    if [[ " ${choices[@]} " =~ " 3 " ]]; then
+        echo "Exiting..."
+        break
+    fi
 
     for choice in "${choices[@]}"; do
         case $choice in
@@ -28,9 +43,9 @@ while true; do
                 echo "Remove Server setup"
                 remove_server
                 ;;
-            3)
-                echo "Back"
-                bash ../setup.sh
+            # 3)
+            #     echo "Back"
+            #     bash ../setup.sh
                 ;;
             *)
                 echo "Invalid option"
@@ -38,18 +53,3 @@ while true; do
         esac
     done
 done
-
-remove_agent(){
-    sudo systemctl stop telegraf.service
-    sudo apt-get remove -y telegraf
-    sudo rm -rf /var/lib/telegraf /etc/telegraf
-}
-
-remove_server(){
-    sudo systemctl stop influxd.service grafana-server.service telegraf.service
-    sudo apt-get remove -y telegraf influxdb2 grafana
-
-    sudo rm -rf /var/lib/influxdb /var/log/influxdb
-    sudo rm -rf /var/lib/telegraf /etc/telegraf
-    sudo rm -rf /var/lib/grafana /etc/grafana /var/log/grafana 
-}
