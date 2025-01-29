172b781382264ceadaa0a35c44bb5b9ca30890d9
jbelascoain <cuentasjuani@gmail.com>
2025-01-29 00:24:11 -0300

    telegraf vars

--- server/influx_telegraf_install.sh
+++ server/influx_telegraf_install.sh
@@ -29,14 +29,13 @@ curl http://$influx_server:$influx_port/api/v2/setup \
 
 # Copy telegraf configuration files
 sudo tee /etc/default/telegraf > /dev/null <<EOF
-influx_user=$user
-influx_password=$password
 influx_token=$influx_token
 influx_bucket=$influx_bucket
 influx_org=$influx_org
-influx_url=$influx_server:$influx_port
+influx_server=localhost
+influx_port=$influx_port
 EOF
-sudo cp ./config/telegraf_server.conf /etc/telegraf/telegraf.conf
+sudo cp ./config/telegraf.conf /etc/telegraf/telegraf.conf
 
 # Start and enable telegraf service
 sudo systemctl enable telegraf
